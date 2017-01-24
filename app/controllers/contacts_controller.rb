64beb25e99a8ebaaf357f55d1750fd7e6beacbc4
John Messina <johnvm914@gmail.com>
2017-01-24 12:55:48 -0500

    implemented all seven RESTful actions

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,41 @@
 class ContactsController < ApplicationController
 
-  def first_contact_method
-    @contact = Contact.first
-    render "first_contact.html.erb"
+  def show
+    @contact = Contact.find_by(id: params[:id])
+    render "show.html.erb"
   end
 
-  def all_contacts_method
+  def index
     @contacts = Contact.all
-    render "all_contacts.html.erb"
+    render "index.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    @contact = Contact.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number])
+    @contact.save
+    render "create.html.erb"
+  end
+
+  def edit
+    @contact = Contact.find_by(id: params[:id])
+    render "edit.html.erb"
+  end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.assign_attributes(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number])
+    @contact.save
+    render "update.html.erb"
+  end
+
+  def destroy
+    @contact = Contact.find_by(id: params[:id])
+    @contact.destroy
+    render "destroy.html.erb"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get "/first_contact", to: "contacts#first_contact_method"
-  get "/all_contacts", to: "contacts#all_contacts_method"
+  get "/contacts/new", to: "contacts#new"
+  get "/contacts/:id", to: "contacts#show"
+  get "/contacts", to: "contacts#index"
+  get "/contacts/:id/edit", to: "contacts#edit"
+  patch "/contacts/:id", to: "contacts#update"
+  delete "/contacts/:id", to: "contacts#destroy"
+  post "/contacts", to: "contacts#create"
 end
 
